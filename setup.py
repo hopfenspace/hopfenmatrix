97680211de83aa8a2dcfa6ce2aafa44fb9728e7a
myOmikron <accounts@omikron.dev>
2020-12-13 23:55:40 +0100

    Updated pypi version to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name="hopfenmatrix",
-    version="0.2.0",
+    version="0.2.1",
     author="Wolfgang Fischer, Niklas Pfister",
     author_email="kontakt@omikron.dev",
     description="A library to make matrix-nio easier",
